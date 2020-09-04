320d8049ce9e3b96f105bff16c55db45d21c4685
Felix A. Gladisch <felix@felixgladisch.de>
2020-09-04 15:21:36 +0200

    ♻️ use lit element class in connect util

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,38 +1,32 @@
+import { LitElement } from "lit-element";
 import { Action, Store, Unsubscribe } from "redux";
 
 type Constructor<T> = new (...args: any[]) => T;
 
-interface CustomElement {
-  connectedCallback?(): void;
-  disconnectedCallback?(): void;
-  readonly isConnected: boolean;
-}
-
 export const connect = <S, A extends Action>(store: Store<S, A>) => <
-  T extends Constructor<CustomElement>
+  T extends Constructor<LitElement>
 >(
   baseElement: T
 ) =>
   class extends baseElement {
-    _storeUnsubscribe!: Unsubscribe;
+    _storeUnsubscribe: Unsubscribe | null = null;
 
     connectedCallback() {
-      if (super.connectedCallback) {
-        super.connectedCallback();
-      }
+      super.connectedCallback();
 
       this._storeUnsubscribe = store.subscribe(() =>
         this.stateChanged(store.getState())
       );
+
       this.stateChanged(store.getState());
     }
 
     disconnectedCallback() {
-      this._storeUnsubscribe();
-
-      if (super.disconnectedCallback) {
-        super.disconnectedCallback();
+      if (this._storeUnsubscribe) {
+        this._storeUnsubscribe();
       }
+
+      super.disconnectedCallback();
     }
 
     stateChanged(_: S) {
